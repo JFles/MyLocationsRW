223f5db5a89bef40da20993cdf07be39a74adfb8
Jeremy Fleshman <jfleshman@gmail.com.com>
2019-06-15 19:32:12 -0500

    - added basic behavior for coordinate reporting - added some error handling and permission requesting ()p.532)

--- MyLocationsRW/CurrentLocationViewController.swift
+++ MyLocationsRW/CurrentLocationViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreLocation
 
-class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
+class CurrentLocationViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var latitudeLabel: UILabel!
     @IBOutlet weak var longitudeLabel: UILabel!
@@ -19,14 +19,115 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
 
     let locationManager = CLLocationManager()
 
+    var location: CLLocation?
+    var updatingLocation = false
+    var lastLocationError: Error?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateLabels()
     }
 
     // MARK:- Actions
     @IBAction func getLocation() {
-        // do nothing yet
+
+        let authStatus = CLLocationManager.authorizationStatus()
+        if authStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+
+        if authStatus == .denied || authStatus == .restricted {
+            showLocationServicesDeniedAlert()
+            return
+        }
+
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
+        updatingLocation = true
+    }
+}
+
+extension CurrentLocationViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager,
+                         didFailWithError error: Error) {
+        print("didFailWithError: \(error.localizedDescription)")
+
+        // locationUnknown error is ignored as the app will continue to try to update
+        if (error as NSError).code == CLError.locationUnknown.rawValue {
+            return
+        }
+        lastLocationError = error
+        stopLocationManager()
+        updateLabels()
+    }
+
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last!
+        print("didUpdateLocations \(newLocation)")
+
+        location = newLocation
+        updateLabels()
+    }
+}
+
+// MARK: - Private Methods
+private extension CurrentLocationViewController {
+    func updateLabels() {
+        if let location = location {
+            latitudeLabel.text = String(format: "%.8f", location.coordinate.latitude)
+            longitudeLabel.text = String(format: "%.8f", location.coordinate.longitude)
+            tagButton.isHidden = false
+            messageLabel.text = ""
+        } else {
+            latitudeLabel.text = ""
+            longitudeLabel.text = ""
+            addressLabel.text = ""
+            tagButton.isHidden = true
+            let statusMessage: String
+            if let error = lastLocationError as NSError? {
+                if error.domain == kCLErrorDomain &&
+                    error.code == CLError.denied.rawValue {
+                    statusMessage = "Location Services Disabled"
+                } else {
+                    statusMessage = "Error Getting Location"
+                }
+            } else if !CLLocationManager.locationServicesEnabled() {
+                statusMessage = "Location Services Disabled"
+            } else if updatingLocation {
+                statusMessage = "Searching..."
+            } else {
+                statusMessage = "Tap 'Get My Location' to Start"
+            }
+            messageLabel.text = statusMessage
+        }
+    }
+
+    func stopLocationManager() {
+        if updatingLocation {
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            updatingLocation = false
+        }
+    }
+}
+
+// MARK: - Helper Methods
+private extension CurrentLocationViewController {
+    func showLocationServicesDeniedAlert() {
+        let alert = UIAlertController(title: "Location Services Disabled",
+                                      message: "Please enable location services for this app in Settings.",
+                                      preferredStyle: .alert)
+
+        let okAction = UIAlertAction(title: "OK",
+                                   style: .default,
+                                   handler: nil)
+
+        alert.addAction(okAction)
+
+        present(alert, animated: true, completion: nil)
     }
 }
 
